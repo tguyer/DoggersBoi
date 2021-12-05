0db6892a26dfd867f789b91f26388fa5cf22abe4
Travis Guyer <tguyer516@gmail.com>
2021-12-05 14:36:38 -0500

    adding main entrypoint

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Discord stuff
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class DoggersClient(discord.Client):
+    """The custom client for the doggers boi bot."""
+
+    async def on_message(self, message):
+        """Filter messages and direct them to their proper functions.
+
+        Fires for every message in the discord."""
+        if message.author.id == self.id:
+            return
+
+    @staticmethod
+    async def on_ready():
+        """Things that happen when the bot boots up."""
+        print(f'{client.user} has connected to Discord!')
+
+client = DoggersClient()
+client.run(TOKEN)
